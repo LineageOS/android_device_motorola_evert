8f13e916393723d46a4f2e1d5f3a46d62cb61141
Erfan Abdi <erfangplus@gmail.com>
2020-07-28 00:08:09 +0430

    evert: Build PocketMode app
    Change-Id: I7e9b03a8c139ff80fa99b8dcc317ab548943ebf5

--- device.mk
+++ device.mk
@@ -88,6 +88,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
 
+# PocketMode
+PRODUCT_PACKAGES += \
+    MotoPocketMode
+
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf \
