9607ea23176533e04da78f0b7602b9fa012cc992
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    evert: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_evert.mk
+++ lineage_evert.mk
@@ -7,7 +7,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/evert/device.mk)
 
 # Device identifiers
-BUILD_FINGERPRINT := motorola/evert/evert:9/PPW29.116-16-30/40d1e:user/release-keys
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := evert
 PRODUCT_MANUFACTURER := motorola
@@ -15,5 +14,6 @@ PRODUCT_MODEL := moto g(6) plus
 PRODUCT_NAME := lineage_evert
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=evert \
-    PRIVATE_BUILD_DESC="evert-user 9 PPW29.116-16-30 40d1e release-keys"
+    BuildDesc="evert-user 9 PPW29.116-16-30 40d1e release-keys" \
+    BuildFingerprint=motorola/evert/evert:9/PPW29.116-16-30/40d1e:user/release-keys \
+    DeviceName=evert
